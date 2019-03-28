43fd6d17a87768bffd338b045648acce86b5cd02
Tommy Wang <Tommy.Wang@coxautoinc.com>
2019-03-28 12:58:40 -0500

    allow for pre-installed pyenv (by brew, presumably), remove auto-install of python

--- pyenv.plugin.zsh
+++ pyenv.plugin.zsh
@@ -1,22 +1,11 @@
-local PYENV_ROOT=$HOME/.pyenv
-local fresh_install=0
+PYENV=$(which pyenv) || PYENV=
 
-[ -d $PYENV_ROOT ] || {
-    git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
-    fresh_install=1
-} > /dev/null
-
-export PATH=$PYENV_ROOT/bin:$PATH
+if [[ -z "$PYENV" ]]; then
+    export PYENV_ROOT=$HOME/.pyenv
+    if [[ ! -d "$PYENV_ROOT" ]]; then
+      git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
+    fi
+    export PATH=$PYENV_ROOT/bin:$PATH
+fi
+  
 eval "$(pyenv init -)"
-
-[ $fresh_install -eq 1 ] && {
-    local all=$(pyenv install -l | tr -d '^ \t')
-    local py27=$(echo $all | \grep -P "^2\.7\.\d+$" | tail -1)
-    local py3=$(echo $all | \grep -P "^3\.\d+\.\d+$" | tail -1)
-    echo "Installing $py27 for pyenv..."
-    pyenv install -s $py27
-    echo "Installing $py3 for pyenv..."
-    pyenv install -s $py3
-    pyenv global $py27 $py3
-    pyenv versions
-}
